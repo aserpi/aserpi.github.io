8092a5877ba740c6f4c77643953ad9b083c520cf
Alessandro Serpi <alessandro.serpi@me.com>
2023-03-09 21:29:37 +0100

    Add work in progress disclaimer

--- src/App.js
+++ src/App.js
@@ -23,17 +23,8 @@ function App() {
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              <Code fontSize="xl">Work in Progress</Code>
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
           </VStack>
 
           <Footer />
