3d558bc092014fea88a3f3f0a00dfa1f3aaa9d9f
Alessandro Serpi <alessandro.serpi@me.com>
2023-03-11 13:57:55 +0100

    Customize ghost button style on hover

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,20 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  components: {
+    Button: {
+      variants: {
+        ghost: {
+          _hover: {
+            background: 'transparent',
+            transitionDuration: '.2s',
+            transform: 'scale(1.2)',
+            transitionTimingFunction: 'ease-in-out',
+          },
+        },
+      },
+    },
+  },
   config: {
     initialColorMode: 'system',
     useSystemColorMode: false,
