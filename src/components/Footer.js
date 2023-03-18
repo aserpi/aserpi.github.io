3b5ebd5177dc5aee8dc36d0337955be62e3295f0
Alessandro Serpi <alessandro.serpi@me.com>
2023-03-18 19:30:11 +0100

    Improve footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,72 +2,70 @@ import {
   Box,
   ButtonGroup,
   Divider,
+  Flex,
   Grid,
   GridItem,
   IconButton,
   Image,
+  Link,
   Text,
 } from '@chakra-ui/react';
 import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 
-import logo from '../logo.svg';
-import { TextLogo } from '../textLogo';
+import logo from '../images/logo.svg';
+import { TextLogo } from './TextLogo';
 
 export const Footer = () => {
   return (
     <Grid
       as="footer"
       columnGap={2}
-      margin="auto"
-      padding={{ base: 4, md: 8 }}
-      templateColumns={{ base: 'auto', md: 'max-content 1fr max-content' }}
+      paddingBottom={6}
+      paddingTop={8}
+      paddingX={2}
+      templateColumns={{ base: 'auto', md: 'auto auto' }}
       templateRows={{ base: 'max-content auto auto', md: 'max-content auto' }}
       role="contentinfo"
-      rowGap={2}
+      rowGap={{ base: 2, md: 6 }}
       width="100%"
     >
-      <GridItem colSpan={{ base: 1, md: 3 }} display="flex" rowStart={1}>
-        <Divider marginBottom={{ base: 2, md: 6 }} />
+      <GridItem
+        as={Flex}
+        colSpan={{ base: 1, md: 2 }}
+        marginBottom={{ base: 4, md: 0 }}
+        rowStart={1}
+      >
+        <Divider />
       </GridItem>
       <GridItem
         alignItems="center"
-        display="flex"
+        as={Flex}
+        colStart={1}
         gap={2}
-        justifyContent="center"
+        justifyContent={{ base: 'center', md: 'left' }}
+        marginLeft={{ base: 0, md: 2 }}
         rowStart={2}
       >
         <Image height={8} src={logo} width="auto" />
-        <Box height={6} width="auto">
+        <Box height={6} width="auto" zIndex="docked">
           <TextLogo height="100%" />
         </Box>
       </GridItem>
       <GridItem
         alignItems="center"
-        display="flex"
-        justifyContent="center"
-        rowStart={{ base: 4, md: 2 }}
-        textAlign="center"
-      >
-        <Text color="subtle" fontSize="sm" margin="auto">
-          &copy; {new Date().getFullYear()} Alessandro Serpi. All rights
-          reserved.
-        </Text>
-      </GridItem>
-
-      <GridItem
-        alignItems="center"
-        display="flex"
-        justifyContent="center"
+        as={Flex}
+        colStart={{ base: 1, md: 2 }}
+        justifyContent={{ base: 'center', md: 'right' }}
         rowStart={{ base: 3, md: 2 }}
       >
-        <ButtonGroup variant="ghost">
+        <ButtonGroup marginRight={{ base: 0, md: 2 }} variant="ghostLight">
           <IconButton
             aria-label={'Email'}
             icon={<FaEnvelope fontSize="1.25rem" />}
             onClick={() => {
               const handle = 'aserpi';
-              const tld = 'it';
               const scheme = 'otliam';
+              const tld = 'it';
               window.location.href = `${scheme
                 .split('')
                 .reverse()
@@ -76,17 +74,33 @@ export const Footer = () => {
           />
           <IconButton
             aria-label="LinkedIn"
-            as="a"
+            as={Link}
             href="https://linkedin.com/in/alessandro-serpi"
             icon={<FaLinkedin fontSize="1.25rem" />}
+            target="_blank"
           />
           <IconButton
             aria-label="GitHub"
-            as="a"
+            as={Link}
             href="https://github.com/aserpi"
             icon={<FaGithub fontSize="1.25rem" />}
+            target="_blank"
           />
         </ButtonGroup>
+      </GridItem>
+      <GridItem
+        alignItems="center"
+        as={Flex}
+        colEnd={{ base: 2, md: 3 }}
+        colStart={1}
+        justifyContent="center"
+        rowStart={{ base: 4, md: 2 }}
+        textAlign="center"
+      >
+        <Text color="subtle" fontSize="sm" margin="auto">
+          &copy; {new Date().getFullYear()} Alessandro Serpi. All rights
+          reserved.
+        </Text>
       </GridItem>
     </Grid>
   );
