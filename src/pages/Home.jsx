d42edf18df18129739c7c1f3b0b70e9ca2e62439
Alessandro Serpi <alessandro.serpi@me.com>
2023-04-27 22:24:55 +0200

    Add home page
    Closes #5.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,72 @@
-import { Box, Code, Grid, Text, VStack } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import { Box, Flex, Grid, Heading, Image, Spacer } from '@chakra-ui/react';
+import { Fragment, useEffect } from 'react';
+import Typewriter from 'typewriter-effect';
 
-import { RotatingLogo } from '../components/RotatingLogo';
+import logo from '../images/logo.svg';
 
 export const Home = () => {
+  const typewriterPause = 2000;
+
   useEffect(() => {
     document.title = 'Home | aserpi';
   }, []);
 
   return (
-    <Box textAlign="center" fontSize="xl">
-      <Grid p={3}>
-        <VStack spacing={8}>
-          <RotatingLogo h="40vmin" pointerEvents="none" />
-          <Text>
-            <Code fontSize="xl">Work in Progress</Code>
-          </Text>
-        </VStack>
+    <Fragment>
+      <Spacer />
+      <Grid
+        templateColumns={{ base: 'auto', md: '1fr auto 1fr' }}
+        templateRows={{ base: 'auto auto auto', md: 'auto' }}
+      >
+        <Flex
+          gridRow={{ base: 3, md: 1 }}
+          justifyContent={{ base: 'center', md: 'end' }}
+        >
+          <Image height={256} src={logo} />
+        </Flex>
+        <Box height={8} gridRow={{ base: 2, md: 1 }} width={8} />
+        <Flex
+          direction="column"
+          gridRow={1}
+          justifyContent="center"
+          textAlign={{ base: 'center', md: 'inherit' }}
+        >
+          <Heading size="lg">Welcome! I&apos;m</Heading>
+          <Heading size="2xl">Alessandro Serpi,</Heading>
+          <Heading marginTop={1} size="lg">
+            <Typewriter
+              onInit={typewriter => {
+                typewriter.rewrite = (str, deleteChars) => {
+                  deleteChars = !Number.isInteger(deleteChars)
+                    ? str.length
+                    : deleteChars >= 0
+                    ? deleteChars
+                    : str.length + deleteChars;
+
+                  return typewriter
+                    .typeString(str)
+                    .pauseFor(typewriterPause)
+                    .deleteChars(deleteChars)
+                    .pauseFor(typewriterPause / 3);
+                };
+
+                typewriter
+                  .rewrite('a cyber security consultant.', -2)
+                  .rewrite('Microsoft Security Operations Analyst.')
+                  .rewrite('Splunk Enterprise Admin.')
+                  .rewrite('Cybereason Threat Hunter.')
+                  .rewrite('Cribl Admin.')
+                  .rewrite('curious mind.', 14)
+                  .typeString('lways learning, always experimenting.')
+                  .pauseFor(typewriterPause * 2)
+                  .start();
+              }}
+              options={{ delay: 75, deleteSpeed: 10, loop: true }}
+            />
+          </Heading>
+        </Flex>
+        <Spacer />
       </Grid>
-    </Box>
+    </Fragment>
   );
 };
